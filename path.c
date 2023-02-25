9797266217924dd242576201b6d726c2e85a1a71
GideonMissoy <missoykipsang@gmail.com>
2023-02-24 18:55:18 -0800

    Adding some files.

---
+++ path.c
@@ -0,0 +1,136 @@
+#include "shell.h"
+
+/**
+ * _strcpy - copies str from src to dest
+ * @src: source string
+ * @dest: destination string
+ * Return: pointer to destination string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int len = 0;
+
+	while (*(src + len) != '\0')
+		len++;
+
+	for (i = 0; i <= len; i++)
+		*(dest + i) = *(src + i);
+
+	return (dest);
+}
+
+
+/**
+ * executepath - function to access PATH
+ * @p: directory in PATH to access
+ * @tokens: array of tokens to check
+ */
+
+void executepath(char *p, char **tokens)
+{
+	int status, len, len2;
+	pid_t child;
+	char *newp = NULL;
+
+	for (len = 0; *(p + len) != '\0'; len++)
+		;
+
+	for (len2 = 0; tokens[0][len2] != '\0'; len2++)
+		;
+
+	newp = malloc(sizeof(char) * (len + len2 + 2));
+
+	_strcpy(newp, p);
+
+	_strcat(newp, "/"); /* concatenates the token onto its dir */
+
+	_strcat(newp, tokens[0]);
+
+	newp[(len + len2 + 1)] = '\0';
+
+	child = fork(); /* forks a child */
+
+	if (child == 0)
+	{
+		if (access(newp, X_OK) == 0) /* checks if we have execute permission */
+			execve(newp, tokens, environ);
+	}
+
+	else
+		while (waitpid(-1, &status, 0) != child) /* waits for child */
+			;
+
+	if (status == 0)
+		errno = 0;
+
+	free(newp);
+}
+
+
+/**
+ * searchpath - function to search PATH
+ * @p: part of PATH to search
+ * @tokens: array of tokens
+ * Return: 0 for success
+ */
+int searchpath(char *p, char **tokens)
+{
+	struct dirent *de;
+	int reached = 0, onpath = -1;
+
+	p = strtok(p, ":"); /* moves to first path */
+
+	while (p != NULL && reached != 1)
+	{
+		DIR *dr = opendir(p); /* opens current directory pointer */
+
+		while ((de = readdir(dr)) != NULL) /* loops through reading directory contents */
+		{
+			if (_strcmp(de->d_name, tokens[0]) == 0)
+			{           /* compares file name to token */
+				executepath(p, tokens);
+				/* if exists, attempt to execute */
+
+				onpath = 0;
+
+				/* breaks loops and sets it on path  */
+				reached = 1;
+				break;
+			}
+		}
+		closedir(dr); /* close the open directory */
+
+		if (reached == 0)
+			p = strtok(NULL, ":");
+	}
+	return (onpath);
+}
+
+/**
+ * findonpath - find PATH in environ
+ * @tokens: token to pass along to PATH
+ * Return: 0 for success
+ */
+int findonpath(char **tokens)
+{
+	int i = 0, onpath;
+	char *p = NULL;
+	char *path = NULL;
+
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (_strncmp("PATH=", environ[i], 5) == 0) /* loops through environ to get path */
+		{
+			path = _strdup(environ[i]);
+			strtok(path, "=");
+			p = strtok(NULL, "="); /* singles out the actual paths */
+			onpath = searchpath(p, tokens); /* search em' */
+			break;
+		}
+	}
+
+	free(path);
+	return (onpath);
+}
---
+++ simpleshell.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+
+/**
+ * main - entry point of the shell, checks interactivity
+ * @argc: arg count
+ * @argv: array of arguments
+ *
+ * Return: 0 on success or other integer
+ */
+int main(int argc, char *argv[])
+{
+	size_t bufsize = 0;
+	char *line = NULL;
+	int i, num_tokens = 0, cmdcount = 1, shell_interaction;
+
+	signal(SIGINT, SIG_IGN); /* Check interactivity */
+	shell_interaction = isatty(STDIN_FILENO);
+
+	if (shell_interaction == 0 && argc == 1) /* if non-interactive */
+	{
+		while (getline(&line, &bufsize, stdin) > 0) /* grabs input */
+		{
+			num_tokens = numcount(line); /* counts the # of tokens */
+			parse(line, num_tokens, argv, cmdcount);
+			line = NULL;
+			/* sends line and numtoken to parser */
+		}
+		free(line);
+		return (errno);
+	}
+
+	while (shell_interaction)/* if interactive */
+	{
+		write(1, "($) ", 4);
+		num_tokens = 0; /* resets token count to 0 */
+		i = getline(&line, &bufsize, stdin); /* grabs input */
+
+		if (i < 0)
+		{
+			free(line);
+			write(1, "\n", 1);
+			/* frees, skips line, and repeats for failed input or CTRL - D */
+			break;
+		}
+
+		num_tokens = numcount(line); /* counts tokens */
+		parse(line, num_tokens, argv, cmdcount);
+		cmdcount++;
+
+		line = NULL;
+	}
+	return (0);
+}
